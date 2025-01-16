0f0b83497c7c81b9e10e753cba8348ba8bbe8b30
tangyuan233 <36312785+tangyuan233@users.noreply.github.com>
2025-01-16 22:41:09 +0800

    update code

--- process_summary.py
+++ process_summary.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path("content")
 INBOX_DIR = "inbox/48 Clippings"
 client = OpenAI(
-    api_key= '${{ secrets.DEEPSEEK_API_KEY }}',  # 从环境变量中获取 API 密钥 
+    api_key= os.environ.get('DEEPSEEK_API_KEY'),  # 从环境变量中获取 API 密钥 
     base_url="https://api.deepseek.com"    # DeepSeek API base url
 )
 
